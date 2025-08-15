29dac8165e37f09b35c70b679b500a2d80531635
Calyne Menezes de Jesus <calyne.office.22@gmail.com>
2025-08-15 23:30:48 +0000

    Corrige variáveis e entradas no SuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -27,13 +27,13 @@ int main()
   scanf("%d\a", &populaçao1);
 
   printf("O numero de pontos turisticos é: \n");
-  scanf("%d", &numero1);
+  scanf("%i", &numero1);
 
   printf("Sua area em km é: \n");
   scanf("%f", &area1);
 
   printf("O seu PIB é: \n");
-  scanf("%.2f", &PIB1);
+  scanf("%f", &PIB1);
 
   printf("\nSeu estado é: A");
   printf("%c\n", estado1);
@@ -42,7 +42,7 @@ int main()
   printf("O nome da cidade é: \n");
   printf("%.9s\n", nome1);
   printf("A populaçao da cidade é: \n");
-  printf("%i\n", populaçao1);
+  printf("%d\n", populaçao1);
   printf("O numero de pontos turisticos é: \n");
   printf("%d\n", numero1);
   printf("Sua area em km é: \n");
@@ -61,25 +61,25 @@ int main()
   printf("\n Carta 2:\n");
 
   printf("O nome do Estado é: \n");
-  scanf(" %c", &Estado2);
+  scanf("%c", &Estado2);
 
   printf("O nome do codigo é: \n");
-  scanf(" %s", Codigo2);
+  scanf("%s", Codigo2);
 
   printf("O nome da cidade é: \n");
-  scanf(" %s", Nome2);
+  scanf("%s", Nome2);
 
   printf("A populaçao da cidade é: \n");
   scanf("%d", &Populaçao2);
 
   printf("O numero de pontos turisticos é: \n");
-  scanf("%d", &Numero2);
+  scanf("%i", &Numero2);
 
   printf("Sua area em km é: \n");
-  scanf("%f", &Area2);
+  scanf("%f", Area2);
 
   printf("O seu pib é: \n");
-  scanf("%.2f", &Pib2);
+  scanf("%f", Pib2);
 
   printf("\nO nome do Estado é: \n");
   printf("%c\n", Estado2);
@@ -88,7 +88,7 @@ int main()
   printf("O nome da cidade é: \n");
   printf("%.12s\n", Nome2);
   printf("A populaçao da cidade é: \n");
-  printf("%f\n", Populaçao2);
+  printf("%d\n", Populaçao2);
   printf("O numero de pontos turisticos é: \n");
   printf("%d\n", Numero2);
   printf("Sua area em km é: \n");
@@ -96,22 +96,6 @@ int main()
   printf("O seu pib é: \n");
   printf("%.2f\n", Pib2);
 
-  return 0;
-}
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Tes
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um p
-
+  
+  return 0;}
