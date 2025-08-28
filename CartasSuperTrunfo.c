ac40f7dac08a58bb977dd2d3ae6be60e0003a4a4
Calyne Menezes de Jesus <calyne.office.22@gmail.com>
2025-08-28 00:26:05 +0000

    "Cadastro de Cartas: Aventureiro"

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,99 +3,127 @@
 int main()
 {
 
-  char estado1 = 'A';
-  char codigo1[5] = "A01";
-  char nome1[9] = "SaoPaulo";
-  int populaçao1 = 12325000;
-  int numero1 = 50;
-  float area1 = 1521.11;
-  float PIB1 = 699.280;
+    char estado1[20];
+    char codigo1[20];
+    char nome1[20];
+    float populaçao1;
+    int numero1;
+    float area1;
+    float PIB1;
+    float Densidade1;
+    float PIB3;
 
-  printf("DESAFIO DE CARTAS SUPER TRUNFO");
-  printf("\nCarta 1: \n");
+    // densidade populacional (float) = populaçao / por area ;
+    // PIB per capita (float) = PIB  / pela populaçao;
 
-  printf("\nSeu estado é:");
-  scanf("%c", &estado1);
+    printf("\nDESAFIO DE CARTAS SUPER TRUNFO\n");
+    printf("\nCarta 1: \n");
 
-  printf("Seu codigo é:  \n");
-  scanf("%s", codigo1);
+    printf("\nSeu estado é:\n");
+    scanf("%s", estado1);
 
-  printf("O nome da  cidade é: \n");
-  scanf("%s", nome1);
+    printf("Seu codigo é:  \n");
+    scanf("%s", codigo1);
 
-  printf("A populaçao da cidade é:  \n");
-  scanf("%d\a", &populaçao1);
+    printf("O nome da  cidade é: \n");
+    scanf("%s", nome1);
 
-  printf("O numero de pontos turisticos é: \n");
-  scanf("%i", &numero1);
+    printf("A populaçao da cidade é:  \n");
+    scanf("%f", &populaçao1);
 
-  printf("Sua area em km é: \n");
-  scanf("%f", &area1);
+    printf("O numero de pontos turisticos é: \n");
+    scanf("%d", &numero1);
 
-  printf("O seu PIB é: \n");
-  scanf("%f", &PIB1);
+    printf("Sua area em km é: \n");
+    scanf("%f", &area1);
 
-  printf("\nSeu estado é: A");
-  printf("%c\n", estado1);
-  printf("Seu codigo é: \n");
-  printf("%s\n", codigo1);
-  printf("O nome da cidade é: \n");
-  printf("%.9s\n", nome1);
-  printf("A populaçao da cidade é: \n");
-  printf("%d\n", populaçao1);
-  printf("O numero de pontos turisticos é: \n");
-  printf("%d\n", numero1);
-  printf("Sua area em km é: \n");
-  printf("%f\n", area1);
-  printf("O seu PIB é: \n");
-  printf("%.2f\n", PIB1);
+    printf("O seu PIB é: \n");
+    scanf("%f", &PIB1);
 
-  char Estado2 = 'B';
-  char Codigo2[4] = "B02";
-  char Nome2[15] = "RiodeJaneiro";
-  int Populaçao2 = 67.48000;
-  int Numero2 = 30;
-  float Area2 = 1200.25;
-  float Pib2 = 300.50;
+    printf("\nSua carta é: \n");
+    printf("Seu estado é: \n");
+    printf("%s\n", estado1);
+    printf("Seu codigo é: \n");
+    printf("%s\n", codigo1);
+    printf("O nome da cidade é: \n");
+    printf("%.9s\n", nome1);
+    printf("A populaçao da cidade é: \n");
+    printf("%f\n", populaçao1);
+    printf("O numero de pontos turisticos é: \n");
+    printf("%d\n", numero1);
+    printf("Sua area em km é: \n");
+    printf("%f\n", area1);
+    printf("O seu PIB é: \n");
+    printf("%.2f\n", PIB1);
 
-  printf("\n Carta 2:\n");
+    
+    Densidade1 =  (populaçao1 / area1);
+    printf("A Densidade Demográfica por km² é: %.2f\n", Densidade1);
 
-  printf("O nome do Estado é: \n");
-  scanf("%c", &Estado2);
-
-  printf("O nome do codigo é: \n");
-  scanf("%s", Codigo2);
-
-  printf("O nome da cidade é: \n");
-  scanf("%s", Nome2);
-
-  printf("A populaçao da cidade é: \n");
-  scanf("%d", &Populaçao2);
-
-  printf("O numero de pontos turisticos é: \n");
-  scanf("%i", &Numero2);
-
-  printf("Sua area em km é: \n");
-  scanf("%f", Area2);
-
-  printf("O seu pib é: \n");
-  scanf("%f", Pib2);
-
-  printf("\nO nome do Estado é: \n");
-  printf("%c\n", Estado2);
-  printf("O nome do codigo é: \n");
-  printf("%.3s\n", Codigo2);
-  printf("O nome da cidade é: \n");
-  printf("%.12s\n", Nome2);
-  printf("A populaçao da cidade é: \n");
-  printf("%d\n", Populaçao2);
-  printf("O numero de pontos turisticos é: \n");
-  printf("%d\n", Numero2);
-  printf("Sua area em km é: \n");
-  printf("%f\n", Area2);
-  printf("O seu pib é: \n");
-  printf("%.2f\n", Pib2);
+    PIB3 = (float) (PIB1 / populaçao1);
+    printf("O PIB Per-Capita é: %.2f\n", PIB3);
 
 
-  
-  return 0;}
+    char Estado2[25];
+    char Codigo2[25];
+    char Nome2[25];
+    float Populaçao2;
+    int Numero2;
+    float Area2;
+    float Pib2;
+    float Densidade2;
+    float PIB4;
+
+    // densidade populacional (float) = populaçao / por area ;
+    // PIB per capita (float) = PIB  / pela populaçao;
+
+    printf("\n Carta 2:\n");
+
+    printf("O nome do Estado é: \n");
+    scanf("%s", Estado2);
+
+    printf("O nome do codigo é: \n");
+    scanf("%s", Codigo2);
+
+    printf("O nome da cidade é: \n");
+    scanf("%s", Nome2);
+
+    printf("A populaçao da cidade é: \n");
+    scanf("%f", &Populaçao2);
+
+    printf("O numero de pontos turisticos é: \n");
+    scanf("%d", &Numero2);
+
+    printf("Sua area em km é: \n");
+    scanf("%f", &Area2);
+
+    printf("O seu pib é: \n");
+    scanf("%f", &Pib2);
+
+    printf("\nO nome do Estado é: \n");
+    printf("%s\n", Estado2);
+    printf("O nome do codigo é: \n");
+    printf("%.3s\n", Codigo2);
+    printf("O nome da cidade é: \n");
+    printf("%.12s\n", Nome2);
+    printf("A populaçao da cidade é: \n");
+    printf("%f\n", Populaçao2);
+    printf("O numero de pontos turisticos é: \n");
+    printf("%d\n", Numero2);
+    printf("Sua area em km é: \n");
+    printf("%f\n", Area2);
+    printf("O seu pib é: \n");
+    printf("%.2f\n", Pib2);
+
+    
+
+     Densidade2 = (float) (Populaçao2 / Area2);
+    printf("A Densidade Demográfica por km² é: %.2f\n", Densidade2);
+
+    PIB4 = (float) (Pib2 / Populaçao2);
+    printf("O PIB Per-Capita é: %.2f", PIB4);
+    
+    return 0;
+}
+
+
